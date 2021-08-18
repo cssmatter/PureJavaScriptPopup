4341074878cd047940962e4c67c56fddb85a845f
cssmatter@gmail.com <cssmatter@gmail.com>
2021-08-18 16:50:53 +0530

    refactored code

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
+
 var model = document.querySelectorAll('.model')
-var closes = document.querySelectorAll('.close')
 var overlay = document.querySelector('.overlay')
 
-
 function openPopup(open){
-    
-    for(i = 0; i < model.length; i++){
-        let currentModel = model[i].getAttribute('data-popup');
-        if(open == currentModel){
-            model[i].style.display = 'block'
-            overlay.style.display = 'block'
-        }
-    }
+    model.forEach(function(trigger){
+            if(trigger.dataset.popup == open) {
+                trigger.style.display = 'block';
+                overlay.style.display = 'block';
+                
+                var closemodel = document.querySelectorAll('.closeModel')
 
+                closemodel.forEach(function(close){
+                    close.addEventListener('click', function(){
+                        trigger.style.display = 'none'
+                        overlay.style.display = 'none';
+                    })
+                })
+            }
+    })
 }
-
-function closePop(){
-    for(i = 0; i < closes.length; i++){
-        let closeModel = closes[i].closest('.model')
-        if(closeModel.style.display == 'block'){
-            closeModel.style.display = 'none'
-            overlay.style.display = 'none'
-        }
-    }
-}
-
-function closePopup(e){
-    closePop()
-}
-
-overlay.addEventListener('click', function(){
-    closePop()
-})
